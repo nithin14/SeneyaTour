3e750261e968455e1b8a762e79dfee257c314efb
Nithin <nithin961114@gmail.com>
2022-07-21 16:17:43 +0530

    Added View Binding.

--- app/src/main/java/com/example/seneyatour/HomeFragment.kt
+++ app/src/main/java/com/example/seneyatour/HomeFragment.kt
@@ -1,6 +1,35 @@
 package com.example.seneyatour
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.seneyatour.databinding.HomeScreenBinding
 
 class HomeFragment: Fragment() {
+    private var _binding: HomeScreenBinding? = null
+    private val binding get() =_binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = HomeScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerview = binding.recyclerView
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
 }
--- app/src/main/java/com/example/seneyatour/MainActivity.kt
+++ app/src/main/java/com/example/seneyatour/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.seneyatour
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Retrieve nav controller from the nav host fragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
